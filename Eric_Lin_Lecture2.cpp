24aaf8026a5088b6eaad74751a6e997b408b1de3
ericl343 <ericl343@nycstudents.net>
2024-09-16 16:27:45 -0400

    d

---
+++ Eric_Lin_Lecture2.cpp
@@ -0,0 +1,33 @@
+//Eric Lin
+//Lecture 2
+//September 16th, 2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    //My Variables
+    int myInt = 3;          // Int
+    float myFlo = 3.12;     // Float
+    double myDou = 6.24;    // Double my Float
+    char myCha = '$';       // Char
+    bool myBoo = false;     // Bool
+    string myStr = "Sup"    // String
+
+    cout << "int: " << myInt << "\n";
+    cout << "float: " << myFlo << "\n";
+    cout << "double: " << myDou << "\n";
+    cout << "char: " << myCha << "\n";
+    cout << "bool: " << myBoo << "\n";
+    cout << "string: " << myStr << "\n";
+
+    int inches;
+    inches = 10;
+
+    cout << inches <<" inch(es) = ";
+    cout << inches / 2 << "feet (foot) and ";
+    cout << inches % 2 << "inch(es)" << endl;
+
+return 0
+}
