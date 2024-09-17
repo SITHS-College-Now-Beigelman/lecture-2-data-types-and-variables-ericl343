9f092631b20481283d5291bbf099bf8dc64c3e67
ericl343 <ericl343@nycstudents.net>
2024-09-17 16:07:28 -0400

    final

--- Eric_Lin_Lecture2.cpp
+++ Eric_Lin_Lecture2.cpp
@@ -2,8 +2,8 @@
 // Lecture 2
 // September 16th, 2024
 
-#include <iostream>
-#include <string>
+#include <iostream>         // Useful
+#include <string>           // Can add strings
 using namespace std;
 
 int main () 
@@ -15,8 +15,9 @@ int main ()
     char myCha = 'X';       // Char
     bool myBoo = false;     // Bool
     string myStr = "Sup";   // String
-    // Display
-    cout << "int: " << myInt << "\n";
+
+    // Display and Print
+    cout << "int: " << myInt << "\n"; 
     cout << "float: " << myFlo << "\n";
     cout << "double: " << myDou << "\n";
     cout << "char: " << myCha << "\n";
@@ -28,6 +29,15 @@ int main ()
 
     cout << feet << " feet (foot) = ";
     cout << feet / 3 << " yard (s) and ";
-    cout << feet % 3 << " feet (foot)" << endl;
-    return 0;
+    cout << feet % 3 << " feet (foot)" << endl;     //Almost the end
+    return 0;                                       //The End...
+    /*
+    int: 3
+    float: 3.12
+    double: 6.24
+    char: X
+    bool: 0
+    string: Sup
+    6 feet (foot) = 2 yard (s) and 0 feet (foot)
+    */
 }
