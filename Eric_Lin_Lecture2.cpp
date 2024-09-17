1889b7e6ef1ce8ac19c38d4225f7ac9b4a7fc6da
ericl343 <ericl343@nycstudents.net>
2024-09-17 15:50:48 -0400

    17

--- Eric_Lin_Lecture2.cpp
+++ Eric_Lin_Lecture2.cpp
@@ -1,20 +1,21 @@
-//Eric Lin
-//Lecture 2
-//September 16th, 2024
+// Eric Lin
+// Lecture 2
+// September 16th, 2024
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main () {
-    //My Variables
+int main () 
+{
+    // My Variables
     int myInt = 3;          // Int
     float myFlo = 3.12;     // Float
     double myDou = 6.24;    // Double my Float
-    char myCha = '$';       // Char
+    char myCha = 'X';       // Char
     bool myBoo = false;     // Bool
-    string myStr = "Sup"    // String
-
+    string myStr = "Sup";   // String
+    // Display
     cout << "int: " << myInt << "\n";
     cout << "float: " << myFlo << "\n";
     cout << "double: " << myDou << "\n";
@@ -22,12 +23,11 @@ int main () {
     cout << "bool: " << myBoo << "\n";
     cout << "string: " << myStr << "\n";
 
-    int inches;
-    inches = 10;
+    int feet;
+    feet = 6;
 
-    cout << inches <<" inch(es) = ";
-    cout << inches / 2 << "feet (foot) and ";
-    cout << inches % 2 << "inch(es)" << endl;
-
-return 0
+    cout << feet << " feet (foot) = ";
+    cout << feet / 3 << " yard (s) and ";
+    cout << feet % 3 << " feet (foot)" << endl;
+    return 0;
 }
